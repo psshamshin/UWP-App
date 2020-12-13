3091811bf405060c03637364176a423f697404f6
psshamshin <57655945+psshamshin@users.noreply.github.com>
2020-12-14 00:18:50 +0300

    new images page

--- App1/ImagesPage.xaml.cs
+++ App1/ImagesPage.xaml.cs
@@ -13,33 +13,18 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
-// Документацию по шаблону элемента "Пустая страница" см. по адресу https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x419
+// Документацию по шаблону элемента "Пустая страница" см. по адресу https://go.microsoft.com/fwlink/?LinkId=234238
 
 namespace App1
 {
     /// <summary>
     /// Пустая страница, которую можно использовать саму по себе или для перехода внутри фрейма.
     /// </summary>
-    public sealed partial class MainPage : Page
+    public sealed partial class ImagesPage : Page
     {
-        public MainPage()
+        public ImagesPage()
         {
             this.InitializeComponent();
-        }
-
-        private void TextBlock_SelectionChanged(object sender, RoutedEventArgs e)
-        {
-
-        }
-
-        private void battleButton_Click(object sender, RoutedEventArgs e)
-        {
-            Frame.Navigate(typeof(BattlePage));
-        }
-
-        private void imagesButton_Click(object sender, RoutedEventArgs e)
-        {
-            Frame.Navigate(typeof(ImagesPage));
         }
     }
 }
